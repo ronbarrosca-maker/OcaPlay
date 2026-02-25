20315136f13c572d4fbdd9bf8a89a06ddeefc789
ronbarrosca <ronbarrosca@gmail.com>
2026-02-25 08:52:18 +0100

    25feb

---
+++ Script.js
@@ -0,0 +1,20 @@
+let numGiocatori = 2;
+function mostraGiocaatori(){
+    document.getElementById('conteggio-giocatori').innerText = numGiocatori;
+}
+function aumentaGiocatori(){
+if(numGiocatori<4){
+ numGiocatori++;
+}
+ mostraGiocaatori();
+}
+function diminuisciGiocatori(){
+    if(numGiocatori>2){
+    numGiocatori--;
+    }
+    mostraGiocaatori();
+}
+function gestisciAvanti() {
+    document.getElementById('finestra-menu').classList.add('nascosto');
+    document.getElementById('finestra-nomi').classList.remove('nascosto');
+}
