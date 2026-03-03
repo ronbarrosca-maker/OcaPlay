f4869b52e3d54da2bcd985122f3091dedfdf7a5c
MalekAbdelaziz <malek.abdelaziz@studenti.itismattei.net>
2026-03-03 11:37:18 +0100

    03.03.26

--- Script.js
+++ Script.js
@@ -1,1 +1,27 @@
-
+let numGiocatori = 2;
+function mostraGiocaatori(){
+    document.getElementById('conteggio-giocatori').innerText = numGiocatori;
+}
+function aumentaGiocatori(){
+if(numGiocatori<4){
+ numGiocatori++;
+}
+ mostraGiocaatori();
+}
+function diminuisciGiocatori(){
+    if(numGiocatori>2){
+    numGiocatori--;
+    }
+    mostraGiocaatori();
+}
+function gestisciAvanti() {
+    document.getElementById('finestra-menu').classList.add('nascosto');
+    document.getElementById('finestra-nomi').classList.remove('nascosto');
+    var n = document.getElementById('conteggio-giocatori').innerText;
+    creacaselle(n);
+}
+function creacaselle(n){
+    for(let i=0;i<n;i++){
+        document.getElementById('listagiocatori').innerHTML=document.getElementById('listagiocatori').innerHTML + "<input type='text' name='giocatore1'></input>";
+    }
+}
